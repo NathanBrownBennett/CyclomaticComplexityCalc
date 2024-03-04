d4b7650a3fe5d23c3541c7a9e00b30a8be1f1fb1
Nathan <46282855+NathanBrownBennett@users.noreply.github.com>
2024-03-04 15:35:13 +0000

    v 0.0.6

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, redirect, url_for, render_template
 import os
+from flask import Flask, request, redirect, url_for, render_template
+import os
+
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'js'}
 
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -11,7 +17,38 @@ def allowed_file(filename):
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template('Index.html')
+
+@app.route('/AboutUs', methods=["GET"])
+def about_us():
+    return render_template('AboutUs.html')
+
+@app.route('/Privacy', methods=["GET"])
+def privacy():
+    return render_template('Privacy.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'script' not in request.files:
+        return redirect(request.url)
+    file = request.files['script']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+    else:
+        return "File not allowed. Please upload a .js file."
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('Index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
