afa85066ad4ea21fb264283ad85afe4b49749128
Nathan <46282855+NathanBrownBennett@users.noreply.github.com>
2024-03-04 20:47:29 +0000

    v 0.1.2 Upload Error now works

--- static/script.js
+++ static/script.js
@@ -6,7 +6,52 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const uploadOverlay = document.getElementById('uploadOverlay');
     const closeButton = document.querySelector('.close-button');
     const uploadTitle = document.querySelector('#uploadForm h2');
+    const fileUpload = document.getElementById('fileUpload');
+    const uploadButton = document.querySelector('#fileUploadForm button');
+    const analysisType = document.getElementById('analysisType');
+    const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
 
+    const saveUploadData = (ip, time, location, device, app, fileName) => {
+        const data = {
+            ip,
+            time,
+            location,
+            device,
+            app,
+            fileName
+        };
+        fetch('/SiteUploads.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+    };
+
+    uploadButton.addEventListener('click', () => {
+        const file = fileUpload.files[0];
+        if (file && file.size <= MAX_FILE_SIZE && ['.py', '.java', '.c', '.cpp', '.cs'].includes(file.name.split('.').pop())) {
+            const formData = new FormData();
+            formData.append('file', file);
+            fetch('/SiteUploads', {
+                method: 'POST',
+                body: formData
+            }).then(response => {
+                if (response.ok) {
+                    // Assuming you have a way to get these values
+                    const ip = 'ip';
+                    const time = new Date().toISOString();
+                    const location = 'location';
+                    const device = 'device';
+                    const app = analysisType.value;
+                    saveUploadData(ip, time, location, device, app, file.name);
+                }
+            });
+        } else {
+            alert('Invalid file. Please select a .py, .java, .c, .cpp, or .cs file that is less than 50MB.');
+        }
+    });
     const closeUploadForm = () => {
         uploadOverlay.style.display = 'none';
     };
